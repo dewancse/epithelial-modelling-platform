35f151682540bac517ff698b0b642b7212039d6e
Dewan Sarwar <dsar941@UoA.auckland.ac.nz>
2016-10-25 12:59:56 +1300

    Added initial setup for search and epithelial platform

--- js/index.js
+++ js/index.js
@@ -1,3 +1,108 @@
 /**
- * Created by dsar941 on 10/25/2016.
+ * Created by dsar941 on 9/8/2016.
  */
+
+var endpoint = "https://models.physiomeproject.org/pmr2_virtuoso_search";
+var query = 'SELECT ?id WHERE { ?id  <http://biomodels.net/biology-qualifiers/isVersionOf> ' +
+    '<http://identifiers.org/go/GO:0005272> }';
+
+var item = [];
+var chkBox, button;
+
+var sparqlQueryJson = function (queryStr, endpoint) {
+
+    var xmlhttp = new XMLHttpRequest();
+
+    xmlhttp.open('POST', endpoint, true);
+    xmlhttp.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xmlhttp.setRequestHeader("Accept", "application/sparql-results+json");
+
+    xmlhttp.onreadystatechange = function () {
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            myCallback(xmlhttp.responseText);
+        }
+    }
+
+    // Send the query to the endpoint.
+    xmlhttp.send(queryStr);
+};
+
+var myCallback = function (str) {
+    // Convert result to JSON
+    var jsonObj = JSON.parse(str);
+
+    console.log(jsonObj);
+
+    var workspaceList = document.getElementById("workspacelist");
+    var list = document.createElement("ul");
+
+    for (var i = 0; i < jsonObj.results.bindings.length; i++) {
+
+        // id
+        var idstr = jsonObj.results.bindings[i].id.value;
+        var index = idstr.search(".cellml");
+        var wname = idstr.slice(0, index);
+
+        var workspaceurl = "https://models.physiomeproject.org/workspace" + "/" + wname + "/" + "@@file" + "/" + "HEAD"
+            + "/" + jsonObj.results.bindings[i].id.value;
+
+        item[i] = document.createElement("li");
+        item[i].id = idstr;
+        item[i].innerHTML = '<a href=' + workspaceurl + ' + target=_blank>' + wname + " / " + idstr + '</a><br>';
+        item[i].innerHTML += '<input id="item[i]" type="checkbox" value="item[i]"><label>&nbsp;&nbsp;Annotations</label><br>';
+
+        workspaceList.appendChild(list.appendChild(item[i]));
+        workspaceList.appendChild(document.createElement("br"));
+    }
+};
+
+$("#ClickMe").click(function () {
+    chkBox = $('input');
+    for (var i = 0; i < item.length; i++) {
+        if (chkBox[i].checked) {
+            var idstr = item[i].id;
+            var n = idstr.search("#");
+            var idn = idstr.slice(n + 1, idstr.length);
+
+            // id
+            var index = idstr.search(".cellml");
+            var wname = idstr.slice(0, index);
+
+            var raw = "https://models.physiomeproject.org/workspace" + "/" + wname + "/" + "rawfile" + "/" + "HEAD"
+                + "/" + idstr;
+
+            rawQueryJson(item[i], raw, idn);
+        }
+    }
+});
+
+var rawQueryJson = function (itemHtml, vEndPoint, id) {
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.open('POST', vEndPoint, true);
+
+    xmlhttp.onreadystatechange = function () {
+
+        if (xmlhttp.readyState == 4 && xmlhttp.status == 200) {
+            var text = xmlhttp.responseText;
+            var parser = new DOMParser();
+            var xmlDoc = parser.parseFromString(text, "text/xml");
+
+            // Look up by variable tag
+            for (var i = 0; i < xmlDoc.getElementsByTagName("variable").length; i++) {
+                if (xmlDoc.getElementsByTagName("variable")[i].getAttribute("cmeta:id") == id) {
+                    itemHtml.innerHTML += '<hr>';
+                    itemHtml.innerHTML += id + '<br>';
+                    itemHtml.innerHTML += xmlDoc.getElementsByTagName("variable")[i].getAttribute("name") + '<br>';
+                    itemHtml.innerHTML += '<hr>';
+                }
+            }
+        }
+    }
+    xmlhttp.send();
+};
+
+$(document).ready(function () {
+
+    //// Make the query.
+    sparqlQueryJson(query, endpoint);
+});
