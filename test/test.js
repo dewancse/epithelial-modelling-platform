279b1f70e2994445bd981ffd09107126228165de
Dewan Sarwar <sarwarcse@gmail.com>
2017-12-19 19:17:03 +1300

    minor change in test.js

--- test/test.js
+++ test/test.js
@@ -3,22 +3,20 @@
  *
  * Automated test cases by Selenium
  */
+"use strict";
 
 var webdriver = require('selenium-webdriver');
-var $ = require('jquery');
 
-var driver_chr = new webdriver.Builder()
+var browser = new webdriver.Builder()
     .forBrowser('chrome')
     .build();
 
-searchTest(driver_chr);
+searchTest(browser);
 
-function searchTest(driver) {
-    driver.get('http://localhost:63342/epithelial-modelling-platform/src/index.html');
-
-    driver.sleep(3000).then(function () {
-
-        driver.getTitle().then(function (title) {
+function searchTest(browser) {
+    browser.get('http://localhost:63342/epithelial-modelling-platform/src/index.html');
+    browser.sleep(3000).then(function () {
+        browser.getTitle().then(function (title) {
             console.log("title: ", title);
             if (title === 'Epithelial Modelling Platform') {
                 console.log('Test passed');
@@ -26,8 +24,39 @@ function searchTest(driver) {
                 console.log('Test failed');
             }
         })
+    })
+
+    browser.findElement(webdriver.By.tagName('#listDiscovery')).click();
+    browser.sleep(3000).then(function () {
+
+        browser.findElement(webdriver.By.css('li#listDiscovery > a')).then(function (link) {
+            console.log("link: ", link);
+        })
+
+        // console.log("$(#listDiscovery): ", document.getElementById("#listDiscovery"));
+        // console.log("$(#listDiscovery): ", $("#listDiscovery").prop("baseURI"));
+    })
+
+    browser.findElement(webdriver.By.tagName('#listModels')).click();
+    browser.sleep(3000).then(function () {
+
+        console.log("listModels!");
 
     })
 
-    driver.quit();
+    browser.findElement(webdriver.By.tagName('#documentation')).click();
+    browser.sleep(3000).then(function () {
+
+        console.log("documentation!");
+
+    })
+
+    browser.findElement(webdriver.By.tagName('#home')).click();
+    browser.sleep(3000).then(function () {
+
+        console.log("home!");
+
+    })
+
+    browser.quit();
 }
